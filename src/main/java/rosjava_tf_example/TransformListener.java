ff05f0b93b3fdfcddbadf94f5bece1f5704c0acd
Martin Günther <mguenthe@uos.de>
2013-12-17 16:06:50 +0100

    added testing code

--- src/main/java/rosjava_tf_example/TransformListener.java
+++ src/main/java/rosjava_tf_example/TransformListener.java
@@ -4,6 +4,7 @@
 
 package rosjava_tf_example;
 
+import org.ros.concurrent.CancellableLoop;
 import org.ros.message.MessageListener;
 import geometry_msgs.TransformStamped;
 import tf.tfMessage;
@@ -28,7 +29,7 @@ public class TransformListener extends AbstractNodeMain {
   }
 
   @Override
-  public void onStart(ConnectedNode node) {
+  public void onStart(final ConnectedNode node) {
     transformer.setPrefix(GraphName.of(node.getParameterTree().getString("~tf_prefix", "")));
     tfSubscriber = node.newSubscriber(GraphName.of("tf"), tf.tfMessage._TYPE); 
     tfSubscriber.addMessageListener(new MessageListener<tfMessage>() {
@@ -39,6 +40,35 @@ public class TransformListener extends AbstractNodeMain {
         }
       }
     });
+
+    // TESTING only
+    node.executeCancellableLoop(new CancellableLoop() {
+
+		@Override
+		protected void loop() throws InterruptedException {
+			geometry_msgs.PoseStamped p = node.getTopicMessageFactory().newFromType(geometry_msgs.PoseStamped._TYPE);
+			p.getHeader().setFrameId("/base_link");
+			p.getPose().getOrientation().setW(1.0);   // make valid quaternion
+			// leaving everything else 0 here
+
+			try {
+				getTransformer().transformPose(GraphName.of("/map"), p);
+				System.out.println(
+						"  x: " + p.getPose().getPosition().getX() +
+						", y: " + p.getPose().getPosition().getY() +
+						", z: " + p.getPose().getPosition().getZ() +
+						" ||| Orientation --- " +
+						"  x: " + p.getPose().getOrientation().getX() +
+						"  y: " + p.getPose().getOrientation().getY() +
+						"  z: " + p.getPose().getOrientation().getZ() +
+						"  w: " + p.getPose().getOrientation().getW()
+						);
+			} catch (java.lang.IllegalStateException e) {
+				System.err.println(e);
+			}
+			Thread.sleep(1000);
+		}
+	});
   }
 
   @Override
